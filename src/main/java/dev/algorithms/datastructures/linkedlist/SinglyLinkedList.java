ba154886394e71c9fff89534338d33d5787fcbdf
Bruno Silveira <65982859+brunosm16@users.noreply.github.com>
2026-07-28 09:56:28 -0300

    Create Node<E> for SinglyLinkedList

---
+++ src/main/java/dev/algorithms/datastructures/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,25 @@
+package dev.algorithms.datastructures.linkedlist;
+
+public class SinglyLinkedList<E> {
+    private static class Node<E> {
+        private E element = null;
+        private Node<E> next = null;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+}
