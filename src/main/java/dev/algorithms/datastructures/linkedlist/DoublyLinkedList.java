8e1ae3fd8f5a14e5be83fabd5c688f0fade6e856
Bruno Silveira <65982859+brunosm16@users.noreply.github.com>
2026-08-04 11:52:17 -0300

    Basic methods for DoublyLinkedList

--- src/main/java/dev/algorithms/datastructures/linkedlist/DoublyLinkedList.java
+++ src/main/java/dev/algorithms/datastructures/linkedlist/DoublyLinkedList.java
@@ -6,7 +6,7 @@ public class DoublyLinkedList<E> {
         private Node<E> prev;
         private Node<E> next;
 
-        public Node(E el, Node<E> pre, Node<E> ne){
+        public Node(E el, Node<E> pre, Node<E> ne) {
             element = el;
             prev = pre;
             next = ne;
@@ -23,5 +23,46 @@ public class DoublyLinkedList<E> {
         public E getElement() {
             return element;
         }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+    }
+
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+    public DoublyLinkedList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty()) {
+            return null;
+        }
+        return header.getNext().getElement();
+    }
+
+    public E last() {
+        if(isEmpty()) {
+            return null;
+        }
+
+        return trailer.getPrev().getElement();
     }
 }
