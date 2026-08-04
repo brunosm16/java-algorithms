6824f2299f428dd20c4f7c5358dc0febce6c7c8a
Bruno Silveira <65982859+brunosm16@users.noreply.github.com>
2026-08-04 15:53:51 -0300

    More unit tests for DoublyLinkedListTest

--- src/test/java/dev/algorithms/datastructures/linkedlist/DoublyLinkedListTest.java
+++ src/test/java/dev/algorithms/datastructures/linkedlist/DoublyLinkedListTest.java
@@ -141,6 +141,60 @@ class DoublyLinkedListTest {
     @Test
     void removeLastEmptyReturnsNull() {
         DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
-        assertNull(list.removeFirst());
+        assertNull(list.removeLast());
+    }
+
+    @Test
+    void removeLastWhenEmptyReturnsNull() {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+
+        assertNull(list.removeLast());
+        assertEquals(0, list.size());
+        assertTrue(list.isEmpty());
+        assertNull(list.first());
+        assertNull(list.last());
+    }
+
+    @Test
+    void removeFirstKeepsLastElement() {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+
+        assertEquals(1, list.removeFirst());
+        assertEquals(2, list.first());
+        assertEquals(3, list.last());
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    void removeLastKeepsFirstElement() {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+
+        assertEquals(3, list.removeLast());
+        assertEquals(1, list.first());
+        assertEquals(2, list.last());
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    void removeFirstAndRemoveLastCanEmptyList() {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+
+        list.addLast(1);
+        list.addLast(2);
+
+        assertEquals(1, list.removeFirst());
+        assertEquals(2, list.removeLast());
+        assertEquals(0, list.size());
+        assertTrue(list.isEmpty());
+        assertNull(list.first());
+        assertNull(list.last());
     }
 }
