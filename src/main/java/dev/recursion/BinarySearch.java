8f1c1b9cc8298473bb8ff816087af9c794d1f63c
Bruno Silveira <65982859+brunosm16@users.noreply.github.com>
2026-08-11 21:13:17 -0300

    BinarySearch Algorithm

--- src/main/java/dev/recursion/BinarySearch.java
+++ src/main/java/dev/recursion/BinarySearch.java
@@ -1,5 +1,25 @@
 package dev.recursion;
 
 public class BinarySearch {
+    public static boolean binarySearch(int[] data, int target) {
+        return binarySearch(data, target, 0, data.length - 1);
+    }
 
+    private static boolean binarySearch(int[] data, int target, int low, int high) {
+        if(low > high) {
+            return false;
+        }
+
+        int mid = (low + high) / 2;
+
+        if(data[mid] == target) {
+            return true;
+        }
+
+        if(target < data[mid]) {
+            return binarySearch(data, target, low, mid - 1);
+        } else {
+            return binarySearch(data, target, mid + 1, high);
+        }
+    }
 }
