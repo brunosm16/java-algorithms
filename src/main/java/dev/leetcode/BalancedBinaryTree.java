ecf211e62d7d976ef021a8a34c5c6334d915135a
Bruno Silveira <65982859+brunosm16@users.noreply.github.com>
2026-08-15 19:41:14 -0300

    BalancedBinaryTree solutions

--- src/main/java/dev/leetcode/BalancedBinaryTree.java
+++ src/main/java/dev/leetcode/BalancedBinaryTree.java
@@ -1,5 +1,8 @@
 package dev.leetcode;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class BalancedBinaryTree {
     /**
      * Definition for a binary tree node.
@@ -53,6 +56,81 @@ public class BalancedBinaryTree {
             int height = 1 + Math.max(leftSubtree[1], rightSubtree[1]);
 
             return new int[]{isBalanced ? 1 : 0, height};
+        }
+    }
+
+    class SolutionIterative {
+        /**
+         * 1. Push nodes into a stack to simulate postorder traversal
+         * 2. Use a dictionary to store the depths of each subtrees
+         * 3. For each Node
+         * - Traverse left subtree when done, do the same for right subtree.
+         * - When both subtrees are done(left, right):
+         * - Check if the tree is balanced, if isn't return false, otherwise
+         * push the depth of trees into dictionary.
+         * -
+         * 4. If traversing the tree does not return false the means the tree is balanced.
+         */
+        public boolean isBalanced(TreeNode root) {
+            Stack<TreeNode> nodes = new Stack<>();
+            HashMap<TreeNode, Integer> depths = new HashMap<>();
+
+            TreeNode currentNode = root;
+            TreeNode lastNode = null;
+
+            while (currentNode != null || !nodes.isEmpty()) {
+                if (currentNode != null) {
+                    nodes.push(currentNode);
+                    currentNode = currentNode.left;
+                } else {
+                    currentNode = nodes.peek();
+
+                    if (currentNode.right == null || lastNode == currentNode.right) {
+                        nodes.pop();
+
+                        int leftSubtree = depths.getOrDefault(currentNode.left, 0);
+                        int rightSubtree = depths.getOrDefault(currentNode.right, 0);
+
+                        boolean isUnbalanced = Math.abs(leftSubtree - rightSubtree) > 1;
+                        if (isUnbalanced) {
+                            return false;
+                        }
+                        depths.put(currentNode, 1 + Math.max(leftSubtree, rightSubtree));
+
+                        lastNode = currentNode;
+                        currentNode = null;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class SolutionBruteForce {
+        public boolean isBalanced(TreeNode root) {
+            if(root == null) {
+                return true;
+            }
+
+            int leftSubtree = height(root.left);
+            int rightSubtree = height(root.right);
+
+            if(Math.abs(leftSubtree - rightSubtree) > 1) {
+                return false;
+            }
+
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+
+        private int height(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            return 1 + Math.max(height(root.left), height((root.right)));
         }
     }
 }
