aaf9b160a2e96c9b5f2fe88a5cf784a4e723d52c
Bruno Silveira <65982859+brunosm16@users.noreply.github.com>
2026-09-04 18:45:25 -0300

    SquaresOfaSortedArray

--- src/main/java/dev/leetcode/SquaresOfaSortedArray.java
+++ src/main/java/dev/leetcode/SquaresOfaSortedArray.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class SquaresOfaSortedArray {
-    // Two Pointers
+    /**
+     * Two Pointers approach
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
     class Solution {
         public int[] sortedSquares(int[] nums) {
             int left = 0;
@@ -25,6 +29,35 @@ public class SquaresOfaSortedArray {
             Collections.reverse(result);
 
             return result.stream().mapToInt(i -> i).toArray();
+        }
+    }
+
+    /**
+     * Two Pointers second approach, use a pointerIndex to store the elements in the result
+     * array maintaining the expected order.
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+    class SecondSolution {
+        public int[] sortedSquares(int[] nums) {
+            int arrayLength = nums.length;
+            int[] result = new int[arrayLength];
+            int left = 0;
+            int right = arrayLength - 1;
+            int pointerIndex = arrayLength - 1;
+
+            while (left <= right) {
+                if (nums[left] * nums[left] < nums[right] * nums[right]) {
+                    result[pointerIndex] = nums[right] * nums[right];
+                    right--;
+                } else {
+                    result[pointerIndex] = nums[left] * nums[left];
+                    left++;
+                }
+                pointerIndex--;
+            }
+
+            return result;
         }
     }
 }
